008c2814b6a5ac76aab7b0e4bd1d7fb3d3f64269
jayedul haque <jayedulhaque15@gmail.com>
2024-10-13 21:37:02 +0600

    Discount Service Created

---
+++ src/Services/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repository;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
+    {
+        ICouponRepositroy _couponRepository;
+        ILogger<DiscountService> _logger;
+        public DiscountService(ICouponRepositroy couponRepositroy, ILogger<DiscountService> logger)
+        {
+            _couponRepository = couponRepositroy;
+            _logger = logger;
+        }
+
+        public override async Task<CouponRequest> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _couponRepository.GetDiscount(request.ProductId);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Discount not found."));
+            }
+            _logger.LogInformation("Discount is retrived for ProductName :{productName},Amount : {amount}", coupon.ProductName, coupon.Amount);
+            return new CouponRequest { ProductId = coupon.ProductId, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount };
+            //return _mapper.Map<CouponRequest>(coupon);
+        }
+    }
+}
